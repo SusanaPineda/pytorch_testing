eb10d77830f1e798d80eccf54e17d223613436c3
Susana <susana.pineda.de.luelmo@gmail.com>
2022-06-10 13:06:02 +0200

    clases separadas para creación de dataset y modificación. Parte del tutorial de segmentación

--- README.md
+++ README.md
@@ -56,5 +56,79 @@ with torch.no_grad():
 
 ### Creación de un dataset a partir de datos propios
 Otro tutorial: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+
 En los datasets propios se deben implementar tres funciones: __init__, __len__ y __getitem__. Las imágenes se almacenan en 
 un directorio y las etiquetas se almacenan separadas en un CSV.
+
+Las transformaciones se implementan como clases para que no sea necesario pasar los parametros de transformación cada vez que se llamen.
+
+Se pueden crear transformaciones compuestas utilizando torchvision.transforms.Compose
+
+
+### TorchVision Object Detection Finetuning Tutorial
+https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
+Dataset --> getitem tiene que devolver un diccionario con la siguiente información:
+* boxes (FloatTensor[N, 4])
+* labels (Int64tensor[N])
+* image_id (Int64Tensor[1])
+* area (Tensor[N])
+* iscrowd (UInt8Tensor[N])
+* masks (UInt8Tensor[N,H,W]) (opcional)
+
+Ejemplo de __getitem__:
+```
+def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        mask = Image.open(mask_path)
+        # convert the PIL Image into a numpy array
+        mask = np.array(mask)
+        # instances are encoded as different colors
+        obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+```
--- custom_dataset.py
+++ custom_dataset.py
@@ -1,24 +1,85 @@
+from __future__ import print_function, division
 import os
+import torch
 import pandas as pd
-from torchvision.io import read_image
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
 
-class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
+import utils
+from datasets import FaceLandmarksDataset, Rescale, RandomCrop, ToTensor
 
-    def __len__(self):
-        return len(self.img_labels)
+if __name__ == '__main__':
+    face_dataset = FaceLandmarksDataset(csv_file='data/faces/face_landmarks.csv',
+                                        root_dir='data/faces/')
 
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+    fig = plt.figure()
+
+    for i in range(len(face_dataset)):
+        sample = face_dataset[i]
+
+        print(i, sample['image'].shape, sample['landmarks'].shape)
+
+        ax = plt.subplot(1, 4, i + 1)
+        plt.tight_layout()
+        ax.set_title('Sample #{}'.format(i))
+        ax.axis('off')
+        utils.show_landmarks(**sample)  # ** desempaquetar diccionarios
+
+        if i == 3:
+            plt.show()
+            break
+
+    scale = Rescale(256)
+    crop = RandomCrop(128)
+    composed = transforms.Compose([Rescale(256),
+                                   RandomCrop(224)])
+
+    # Apply each of the above transforms on sample.
+    fig = plt.figure()
+    sample = face_dataset[65]
+    for i, tsfrm in enumerate([scale, crop, composed]):
+        transformed_sample = tsfrm(sample)
+
+        ax = plt.subplot(1, 3, i + 1)
+        plt.tight_layout()
+        ax.set_title(type(tsfrm).__name__)
+        utils.show_landmarks(**transformed_sample)
+
+    plt.show()
+
+    # Crear el dataset con transformaciones:
+    transformed_dataset = FaceLandmarksDataset(csv_file='data/faces/face_landmarks.csv',
+                                               root_dir='data/faces/',
+                                               transform=transforms.Compose([
+                                                   Rescale(256),
+                                                   RandomCrop(224),
+                                                   ToTensor()
+                                               ]))
+
+    # esto se cambia por un Dataloader:
+    """for i in range(len(transformed_dataset)):
+        sample = transformed_dataset[i]
+
+        print(i, sample['image'].size(), sample['landmarks'].size())
+
+        if i == 3:
+            break"""
+
+    dataloader = DataLoader(transformed_dataset, batch_size=4,
+                            shuffle=True, num_workers=0)
+
+    for i_batch, sample_batched in enumerate(dataloader):
+        print(i_batch, sample_batched['image'].size(),
+              sample_batched['landmarks'].size())
+
+        # observe 4th batch and stop.
+        if i_batch == 3:
+            plt.figure()
+            utils.show_landmarks_batch(sample_batched)
+            plt.axis('off')
+            plt.ioff()
+            plt.show()
+            break
